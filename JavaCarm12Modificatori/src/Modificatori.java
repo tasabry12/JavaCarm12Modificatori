7d5f6b40df82bd466f227356d4334d3ad7e42c2e
v-matoscano <v-matoscano@ROMR90F84D0.SEA.CORP.EXPECN.com>
2015-07-22 17:23:00 +0200

    Initial commit

---
+++ JavaCarm12Modificatori/src/Modificatori.java
@@ -0,0 +1,11 @@
+
+public class Modificatori {
+
+	/* MODIFICATORI DI ACCESSO
+	 * MOD.         STESSA CLASSE     STESSO PACKAGE    SOTTOCLASSE    DAPPERTUTTO
+	 * public             SI                SI               SI             SI      
+	 * protected          SI                SI               SI             NO
+	 * default            SI                SI               NO             NO
+	 * private            SI                NO               NO             NO
+	 */
+}
